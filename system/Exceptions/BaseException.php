bb3d3a97b704645747e256f7ebdb9bc21abc9b9f
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-04-27 17:53:38 +0300

    Defining the base exception class

---
+++ system/Exceptions/BaseException.php
@@ -0,0 +1,30 @@
+<?php namespace Core\Exceptions;
+
+class BaseException extends \Exception {
+
+	/**
+	 *This constructor function set the default values for the properties above
+	 *
+	 *@param 
+	 *
+	 */
+	public function __construct()
+	{
+		//
+
+	}
+
+	/**
+	 *This method displays the error message
+	 *
+	 *@param 
+	 *
+	 */
+	public function errorShow()
+	{
+		//
+
+	}
+
+	
+}
