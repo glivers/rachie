55dc35410d2330441bba776f8d87ff31bb68f11b
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-04-29 11:47:09 +0300

    Creating public access methods to the MySQL Query Generation Class

--- system/Drivers/Database/MySQLQuery.php
+++ system/Drivers/Database/MySQLQuery.php
@@ -66,7 +66,7 @@ class MySQLQuery {
 	 *@var
 	 *@read
 	 */
-	protected $where = array();
+	protected $wheres = array();
 
 	/**
 	 *This method quotes input data according to how MySQL woudl expect it
@@ -238,6 +238,514 @@ class MySQLQuery {
 		//set the directions property
 		$this->directions = $direction;
 
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	public function where()
+	{
+		//get the arguments
+		$arguments = func_get_args();
+
+		//throw exception if no arguments passed for the where clause
+		if ( sizeof($arguments < 1) ) 
+		{
+			//throw exception
+			throw new DbException("No arguments passed for the where clause", 1);
+
+		}
+
+		//
+		$arguments[0] = preg_replace("#\?#", "%s", $arguments[0]);
+
+		//loop through the arguments array
+		foreach (array_slice($arguments, 1, null, true) as $i => $parameter) 
+		{
+			//quote the argument contect
+			$arguments[$i] = $this->quote($arguments[$i]);
+
+		}
+
+		//populate the wheres array
+		$this->wheres[] = call_user_func_array("sprintf", $arguments);
+
+		//return this object instance
+		return $this;	
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	protected function buildSelect()
+	{
+		//define a container fields array
+		$fields = array();
+
+		//
+		$where = $order = $limit = $join ="";
+
+		//
+		$template = "SELECT %s FROM %s %s %s %s %s";
+
+		//loop through the fields
+		foreach ($this->fields as $table => $tableFields) 
+		{
+			//loop through the fields checking for aliases
+			foreach ($tableFields as $field => $alias) 
+			{
+				//if field is string, return field name
+				if ( is_string($field) )
+				{
+					//add to fields array
+					$fields[] = "{$field} AS {$alias}";
+
+				}
+				//not an array, return alias
+				else
+				{
+					//get alias as field name
+					$fields[] = $alias;
+
+				}
+
+			}
+
+		}
+
+		//turn the fields into a string
+		$fields = join(", ", $fields);
+
+		//get the query joins
+		$queryJoin = $this->joins;
+
+		//check if the join parameter is empty
+		if ( ! empty($queryJoin) ) 
+		{
+			//add the joins
+			$join = join(" ", $queryJoin);
+
+		}
+
+		//get the where clause
+		$queryWhere = $this->wheres;
+
+		//check if the wheres clause is empty
+		if ( ! empty($queryWhere) ) 
+		{
+			//append where clause
+			$joined = join(" AND ", $queryWhere);
+
+			//assign string to $where var
+			$where = "WHERE {$joined}";
+
+		}
+
+		//get the order clause
+		$queryOrder = $this->orders;
+
+		//check if the orders clause is empty
+		if ( ! empty($queryOrder) ) 
+		{
+			//get the direction if order
+			$orderDirection = $this->directions;
+
+			//assign string to order variable
+			$order = "ORDER BY {$queryOrder} {$orderDirection}";
+
+		}
+
+		//get the limits clause
+		$queryLimit = $this->limits;
+
+		//check if the limits clause is empty
+		if ( ! empty($queryLimit) ) 
+		{
+			//get the offset
+			$limitOffset = $this->offset;
+
+			//if offset has been defined
+			if ( $limitOffset ) 
+			{
+				//get the limit string with offset
+				$limit = "LIMIT {$queryLimit}, {$limitOffset}";
+
+			}
+			//limit offset undefined
+			else
+			{
+				//get the limit string
+				$limit = "LIMIT {$queryLimit}";
+
+			}
+
+		}
+
+		//return the formated query string in the format of $template
+		return sprintf($template, $fields, $$this->froms, $join, $where, $order, $limit);
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	protected function buildInsert($data)
+	{
+		//define a fields container array
+		$fields = array();
+
+		//define a values container array
+		$values = array();
+
+		//define a template format for query
+		$template = "INSERT INTO '%s' ('%s') VALUES (%s)";
+
+		//loop through the input data
+		foreach ($data as $field => $value) 
+		{
+			//polulate the fields array
+			$fields[] = $field;
+
+			//populate the values array
+			$values = $this->quote($value);
+
+		}
+
+		//convert strings array to string
+		$fields = join("', '", $fields);
+
+		//convert values array to string
+		$values = join(", ", $values);
+
+		//return the formated query string
+		return sprintf($template, $this->froms, $fields, $values);
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	protected function buildUpdate($data)
+	{
+		//define the parts container array
+		$parts -= array();
+
+		//define the continer vars initializing to empty
+		$where = $limit = '';
+
+		//define the template format string
+		$format = "UPDATE %s SET %s %s %s";
+
+		//loop through the input data array, populating the parts array
+		foreach ($data as $field => $value) 
+		{
+			//populate the parts array
+			$parts[] = "{$field}=" . $this->quote($value);
+
+		}
+
+		//convert parts array into string
+		$parts = join(", ", $parts);
+
+		//get the where clause
+		$queryWhere = $this->wheres;
+
+		//check if where clause is empty
+		if ( ! empty($queryWhere) ) 
+		{
+			//convert where clause array to string
+			$joined = join(", ", $queryWhere);
+
+			//set the where var valus
+			$where = "WHERE {$joined}";
+
+		}
+
+		//get the limits var value
+		$queryLimit = $this->limits;
+
+		//check if the limit clause is empty
+		if ( ! empty($queryLimit) ) 
+		{
+			//get the offset value
+			$limitOffset = $this->offset;
+
+			//compose limit string
+			$limit = "LIMIT {$queryLimit} {$limitOffset}";
+
+		}
+
+		//return the formated query string
+		return sprintf($template, $this->froms, $parts, $where, $limit);
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	protected function buildDelete()
+	{
+		//initialize $where and $limit container vars to empty strings
+		$where = $limit = '';
+
+		//define the string formating template
+		$template = "DELETE FROM %s %s %s";
+
+		//get the where clause variable
+		$queryWhere = $this->wheres;
+
+		//check if where clause is empty
+		if ( ! empty($queryWhere) ) 
+		{
+			//convert where clause array to string
+			$joined = join(", ", $queryWhere);
+
+			//compose the where string
+			$where = "WHERE {$joined}";
+
+		}
+
+		//get the limit clause variable
+		$queryLimit = $this->limits;
+
+		//check if limit clause is empty
+		if ( ! empty($queryLimit) ) 
+		{
+			//get the offset value
+			$limitOffset = $this->offset;
+
+			//compose the limit clause string
+			$limit = "LIMIT {$queryLimit} {$limitOffset}";
+
+		}
+
+		//return the formated query string
+		return sprintf($template, $this->froms, $where, $limit);
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	public function save($data)
+	{
+		//get the size of the wheres parameter
+		$doInsert = sizeof($this->wheres) == 0;
+
+		//check if doInsert is true
+		if ( $doInsert ) 
+		{
+			//get insert query string
+			$sql = $this->buildInsert($data);
+
+		}
+
+		//not insert, this should be an update
+		else
+		{
+			//get update query string
+			$sql = $this->buildUpdate($data);
+
+		}
+
+		//excecute query
+		$result = $this->connector->excecute($sql);
+
+		//check if query execution failure
+		if ( $result === false) 
+		{
+			//throw exception 
+			throw new MySQLException();
+
+		}
+
+		//if this was an insert, get the insert id
+		if( $doInsert )
+		{
+			//get last insert id and retunr
+			return $this->connector->lastInsertId();
+		}
+
+		//if this was update and sucess, return 0 to show operation successful
+		return 0;
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	public function delete()
+	{
+		//build the delete query string
+		$sql = $this->buildDelete();
+
+		//execute the query string
+		$result = $this->connector->execute($sql);
+
+		//throw error if there was an error perfrorming this query
+		if ( $result === false ) 
+		{
+			//throw excepton
+			throw new MySQLException();
+			
+		}
+
+		//if delete was successfull, get and return the number of affected rows
+		return $this->connector->affectedRows();
+
+	}
+
+	/**
+	 *
+	 *
+	 */
+	public function first()
+	{
+		//get the limit clause value
+		$limit = $this->limits;
+
+		//get the offset value
+		$limitOffset = $this->offset;
+
+		//set the limit to 1
+		$this->limit(1);
+
+		//get all
+		$all = $this->all();
+
+		//get the first
+		$first = ArrayUtility::first($all);
+
+		//if limit is defined
+		if ( $limit ) 
+		{
+			//set the limit property
+			$this->limits = $limit;
+
+		}
+
+		//if limit offset is set
+		if ( $limitOffset ) 
+		{
+			//set the offset
+			$this->offset = $limitOffset;
+
+		}
+
+		//return the first query row
+		return $first;
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	public function count()
+	{
+		//get the limit clause value
+		$limit = $this->limits;
+
+		//get the offset value
+		$limitOffset = $this->offset;
+
+		//get the fields clause
+		$fields = $this->fields;
+
+		//compose the fields array
+		$this->fields = array($this->froms => array("COUNT(1)" => "rows"));
+
+		//set limit to 1
+		$this->limit(1);
+
+		//get the first row
+		$row = $this->first();
+
+		//set the value of the fields
+		$this->fields = $fields;
+
+		//check if fields is defined
+		if ( $fields ) 
+		{
+			//set the value o fields
+			$this->fields = $fields;
+
+		}
+
+		//check if the limit value is set
+		if ( $limit ) 
+		{
+			//set the value of limit
+			$this->limits = $limit;
+
+		}
+
+		//check if the limit offset value is defined
+		if ( $offset ) 
+		{
+			//set the offset value
+			$this->offset = $offset;
+
+		}
+
+		//return the rows array
+		return $row['rows'];
+
+	}
+
+	/**
+	 *
+	 *
+	 *
+	 */
+	public function all()
+	{
+		//build the select query
+		$sql = $this->buildSelect();
+
+		//execute the sql query
+		$result = $this->connector->excecute($sql);
+
+		//check if the query return an error and throw exception
+		if ( $result === false ) 
+		{
+			//get the erro message
+			$error = $this->connector->lastError();
+
+			//throw exception
+			throw new MySQLException("There was an error with your SQL Query : {$error}");
+
+		}
+
+		//define container rows() array
+		$rows = array();
+
+		//loop through the result object composing the rows array
+		for( $i = 0; $i < $result->num_rows; $i++ )
+		{
+			//add to the rows array
+			$rows[] = $result->fetch_array(MYSQLI_ASSOC);
+
+		}
+
+		//return the full rows array
+		return $rows;
 	}
 
 }
