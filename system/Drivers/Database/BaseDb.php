f323fbefb757a99a25b8e35879c8ec179973de68
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-04-29 04:07:14 +0300

    Writing the Base Database Driver Class

---
+++ system/Drivers/Database/BaseDb.php
@@ -0,0 +1,19 @@
+<?php namespace Core\Drivers\Database;
+
+/**
+ *This class is the base database class which all subclasses extend
+ *
+ *@author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Oliver
+ *@category Core
+ *@package Core\Drivers\Database
+ *@link https://github.com/gliver-mvc/gliver
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 1.0.1
+ */
+
+use Core\Drivers\Database\DbImplement;
+class BaseDb {
+
+
+}
