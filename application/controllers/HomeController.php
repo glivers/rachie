114aa58eb3971ce3a4e43d6fdc0c7066a07ab037
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-04-29 11:55:44 +0300

    Writing the default controller for sample application

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -1,14 +1,13 @@
 <?php namespace Controllers;
 
 use Core\Helpers\View;
-use Core\Helpers\Url;
 
 class HomeController extends Controller {
 
     public function index()
     {
-        
-        View::render('sample', array('title' => 'This is the homepage'));
+        //load the homepage
+        View::render('index');
 
     }
 
