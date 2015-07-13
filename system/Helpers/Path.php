ba731324d008ab32b5cdf61f53016fcde4b64b96
Geoffrey Oliver <geoffreyokongo2@gmail.com>
2015-07-13 16:00:40 -0700

    Defining the Path Helper CLass as Static to avoid Cloning of this Class

--- system/Helpers/Path.php
+++ system/Helpers/Path.php
@@ -8,6 +8,24 @@
 class Path {
 
 	/**
+	 *This is the constructor class. We make this private to avoid creating instances of
+	 *this object
+	 *
+	 *@param null
+	 *@return void
+	 */
+	private function __construct() {}
+
+	/**
+	 *This method stops creation of a copy of this object by making it private
+	 *
+	 *@param null
+	 *@return void
+	 *
+	 */
+	private function __clone(){}
+
+	/**
 	 *This method defines the path to the application folder
 	 *
 	 */
