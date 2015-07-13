ea155cc0315e8a8f3c9c38518fec4dd385962fb4
Geoffrey Oliver <geoffreyokongo2@gmail.com>
2015-07-13 14:17:15 -0700

    Making the ArrayHelper class static

--- system/Helpers/Array.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Helpers;
-
-class Array {
-
-	
-}
---
+++ system/Helpers/ArrayHelper.php
@@ -0,0 +1,35 @@
+<?php namespace Helpers;
+
+/**
+ *This class handles all array related functionality
+ *
+ *@author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Oliver
+ *@category Core
+ *@package Core\Helpers\ArrayHelper
+ *@link https://github.com/gliver-mvc/gliver
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 1.0.1
+ */
+
+class ArrayHelper {
+
+	/**
+	 *This is the constructor class. We make this private to avoid creating instances of
+	 *this object
+	 *
+	 *@param null
+	 *@return void
+	 */
+	private function __construct() {}
+
+	/**
+	 *This method stops creation of a copy of this object by making it private
+	 *
+	 *@param null
+	 *@return void
+	 *
+	 */
+	private function __clone(){}
+	
+}
