51d5ecd87d69cedc0b6320aa48bfd2ac229fc439
Geoffrey Oliver <geoffreyokongo2@gmail.com>
2015-07-13 15:59:51 -0700

    Defining the Input Class as Statuc to avoid cloning of this Class

--- system/Helpers/Input.php
+++ system/Helpers/Input.php
@@ -15,6 +15,24 @@
 use Helpers\Url;
 
 class Input {
+
+	/**
+	 *This is the constructor class. We make this private to avoid creating instances of
+	 *this object
+	 *
+	 *@param null
+	 *@return void
+	 */
+	private function __construct() {}
+
+	/**
+	 *This method stops creation of a copy of this object by making it private
+	 *
+	 *@param null
+	 *@return void
+	 *
+	 */
+	private function __clone(){}
 	
 	/**
 	 *This method return current $_POST or $_GET data
