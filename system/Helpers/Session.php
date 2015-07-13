37a55d6283894b7f45914f63b2c241bf3550b7bb
Geoffrey Oliver <geoffreyokongo2@gmail.com>
2015-07-13 15:56:40 -0700

    Defining the Session Helper Class and Static to avoid Cloning

--- system/Helpers/Session.php
+++ system/Helpers/Session.php
@@ -14,6 +14,24 @@
 class Session {
 
 	/**
+	 *This is the constructor class. We make this private to avoid creating instances of
+	 *this object
+	 *
+	 *@param null
+	 *@return void
+	 */
+	private function __construct() {}
+
+	/**
+	 *This method stops creation of a copy of this object by making it private
+	 *
+	 *@param null
+	 *@return void
+	 *
+	 */
+	private function __clone(){}
+
+	/**
 	 *This method initializes the session environment.
 	 *@param null
 	 *@return void
