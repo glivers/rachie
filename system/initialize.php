e4c95f7cea8d6c0d84485f750bc89719011a918b
Geoffrey Oliver <geoffreyokongo2@gmail.com>
2015-07-15 01:37:24 -0700

    Writing file to initialize Registry Settings

---
+++ system/initialize.php
@@ -0,0 +1,16 @@
+<?php
+
+//store this application configuration and free memory
+Drivers\Registry::setConfig($config);
+
+//free memory of this variable
+unset($config);
+
+//store the database settings in the registry and free memory
+Drivers\Registry::setSettings($database);
+
+//free memory of this variable
+unset($database);
+
+//store the request URI and free the memory
+Drivers\Registry::setUrl( ( isset($_GET['url']) ) ? $_GET['url'] : '');
