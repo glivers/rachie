e6fb89bd1dbea0a9bb9067b0bcad3251299f4df8
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-08-14 16:41:34 +0300

    Defining the base exception handler class to be extended by all exception classes

--- system/Exceptions/BaseException.php
+++ system/Exceptions/BaseException.php
@@ -1,13 +1,27 @@
 <?php namespace Exceptions;
 
+/**
+ *This class handles all exceptions thrown and is extended by other classes
+ *
+ *@author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Oliver
+ *@category Core
+ *@package Core\Exceptions\BaseException
+ *@link https://github.com/gliver-mvc/gliver
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 1.0.1
+ */
+
 use Helpers\Path;
 
 class BaseException extends \Exception {
 
 	/**
-	 *This method displays the error message
+	 *This method displays the error message passed from the thrown error
 	 *
-	 *@param 
+	 * @param null
+	 * @return void
+	 * @throws This method does not throw an error
 	 *
 	 */
 	public function show()
@@ -22,7 +36,7 @@ class BaseException extends \Exception {
 		$msg = $this->getCode() . ': Error on line '.$this->getLine().' in '.$this->getFile() .': <b>  "'.$this->getMessage().' " </b> ';
 
 		//load the template file
-		include Path::sys() . 'Exceptions' . DIRECTORY_SEPARATOR . 'index.php';
+		include Path::sys() . 'Exceptions/index.php';
 
 	}
 
