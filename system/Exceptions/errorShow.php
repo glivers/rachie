7dbc88c9ac50f8138c0f347a8c9998c70b66cf1a
GeoffreyOliver <geoffreyokongo2@gmail.com>
2015-08-14 16:36:52 +0300

    Writing hte view file for showing error message

---
+++ system/Exceptions/errorShow.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+	<title><?php echo $title; ?> </title>
+
+</head>
+<body>
+
+<style type="text/css">
+	
+	.container {
+
+		width : 960px;
+		min-height: 100px;
+		background-color: rgba(0,0,0,0.08);
+		font-size: 16px;
+		margin: auto;
+		color: rgba(0, 128, 0, 1);
+	}
+
+</style>	
+
+<div class="container">
+	
+	<p><?php echo $errorMessage; ?></p>
+
+</div>
+</body>
+</html>
+
+
+
