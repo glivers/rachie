77bc7cb31290e39ad7745fb8e19834515b17383b
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-02 01:38:47 -0800

    Modifying the HelperException class to properly inherit from the BaseExceptionClass

--- system/Helpers/Exceptions/HelperException.php
+++ system/Helpers/Exceptions/HelperException.php
@@ -12,46 +12,6 @@
  * @version 1.0.1
  */
 
-use Drivers\Registry;
-use Exceptions\BaseException;
+use Exceptions\BaseExceptionClass;
 
-class HelperException extends BaseException {
-
-	/**
-	 *This method displays the error message passed from the thrown error
-	 *
-	 * @param string The error message to display
-	 * @return void
-	 * @throws This method does not throw an error
-	 *
-	 */
-	public function showError($message = null)
-	{
-		//get the site title
-		$title = Registry::getConfig()['title'];
-
-		//define the error message
-		$errorMessage  = ( $message == null ) ? $this->getCode() . ': Error on line '.$this->getLine().' in '.$this->getFile() .': <b>  "'.$this->getMessage().' " </b> ' : $message;
-		
-		//the variable to be populated with the error message
-		//$errorMessage = $this->getCode() . ': Error on line '.$this->getLine().' in '.$this->getFile() .': <b>  "'.$this->getMessage().' " </b> ';
-
-		//display errors if in development environment
-		if (  DEV == TRUE ) 
-		{
-			//load the showError file
-			include Registry::$errorFilePath . 'errorShow.php';
-
-		}
-		//production environment, hide error
-		else
-		{
-			//load the hide error file
-			include Registry::$errorFilePath . 'errorHide.php';
-
-		}
-
-	}
-
-	
-}
+class HelperException extends BaseExceptionClass {}
