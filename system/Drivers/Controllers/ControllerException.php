3a5f1e5098b81bc939f96368a3012239f665c494
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-02 03:20:13 -0800

    Adding the ControllerException class by extending the BaseExceptionClass to enable custom error handling in the BaseControllerClass.

---
+++ system/Drivers/Controllers/ControllerException.php
@@ -0,0 +1,17 @@
+<?php namespace Helpers\Exceptions;
+
+/**
+ *This class handles all exceptions thrown by Controller Classes
+ *
+ * @author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ * @copyright 2015 - 2020 Geoffrey Oliver
+ * @category Exceptions
+ * @package Drivers\Exceptions\ControllerException
+ * @link https://github.com/gliver-mvc/gliver
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @version 1.0.1
+ */
+
+use Exceptions\BaseExceptionClass;
+
+class ControllerException extends BaseExceptionClass {}
