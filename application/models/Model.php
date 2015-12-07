ed39e5a8f34afd7757182b14784ee861a186f68c
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-07 07:23:16 -0800

    Adding metadata to the Model.php class to better describe the function of this class

--- application/models/Model.php
+++ application/models/Model.php
@@ -1,5 +1,16 @@
 <?php namespace Models;
 
+/**
+ *This is the base Model from which all named models derive by extending
+ *@author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Oliver
+ *@category Models
+ *@package Models\BaseModel
+ *@link https://github.com/gliver-mvc/gliver
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 1.0.1
+ */
+
 use Drivers\Models\BaseModelClass;
 
 class Model extends BaseModelClass {}
