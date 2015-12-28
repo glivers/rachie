677be9272bd78c095f2ed3bfe7684dc3c5df5fff
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-28 14:48:00 +0300

    Creating the method for setting the controller trait basic properties at start

--- system/Drivers/Controllers/BaseControllerTrait.php
+++ system/Drivers/Controllers/BaseControllerTrait.php
@@ -11,12 +11,19 @@
  *@version 1.0.1
  */
 
+use Drivers\Registry;
+
 trait  BaseControllerTrait {
 
 	/**
 	*@var float The request start time
 	*/
 	public $request_start_time;
+
+	/**
+	*@var string The site title as defined in the config
+	*/
+	public $site_title;
 
 	/**
 	 *This magic method determines the request method and composes a method based on the value.
@@ -37,6 +44,25 @@ trait  BaseControllerTrait {
 		if (method_exists($this, $method)) return $method;
 		else return false;
 
+	}
+
+	/**
+	*this method sets the basic app properties in controller
+	*
+	*@param null
+	*@return \Object $this instance of the controller for the purposes of chaining
+	*/
+	public function set_gliver_fr_controller_trait_properties()
+	{
+		//set the request_start_time
+		$this->request_start_time = Registry::$gliver_app_start;
+
+		//set the site title
+		$this->site_title = Registry::getConfig()['title'];
+
+		//return this object instance
+		return $this;
+		
 	}
 
 }
