e6219e40d0d1e00674094402465334e344406b05
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2015-12-31 02:35:17 +0300

    Creating the Upload Helper class

---
+++ system/Helpers/Upload/UploadException.php
@@ -0,0 +1,17 @@
+<?php namespace Helpers\Upload;
+
+/**
+ *This class handles all exceptions thrown by Upload Class
+ *
+ * @author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ * @copyright 2015 - 2020 Geoffrey Oliver
+ * @category Exceptions
+ * @package Helpers\Upload\UploadException
+ * @link https://github.com/gliver-mvc/gliver
+ * @license http://opensource.org/licenses/MIT MIT License
+ * @version 1.0.1
+ */
+
+use Helpers\Exceptions\HelperException;
+
+class UploadException extends HelperException {}
---
+++ system/Helpers/Upload/UploadResponseClass.php
@@ -0,0 +1,15 @@
+<?php namespace Helpers\Upload;
+
+/**
+ *This class is the response object for all successful upload attempts
+ *
+ *@author Geoffrey Oliver <geoffrey.oliver2@gmail.com>
+ *@copyright 2015 - 2020 Geoffrey Oliver
+ *@category Helpers
+ *@package Helpers\Upload
+ *@link core.gliver.io
+ *@license http://opensource.org/licenses/MIT MIT License
+ *@version 1.0.1
+ */
+
+class UploadResponseClass {}
