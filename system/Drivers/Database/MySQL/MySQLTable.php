0562bdb0d80761a3158d33f63f8c110adc9dcce9
Geoffrey Okongo <geoffreyokongo2@gmail.com>
2016-01-10 11:01:36 +0300

    Writing methods for creating and updating MySQL table structures

--- system/Drivers/Database/MySQL/MySQLTable.php
+++ system/Drivers/Database/MySQL/MySQLTable.php
@@ -82,6 +82,12 @@ class MySQLTable {
 	protected $primaries = array();
 
 	/**
+	 * @var array Whether the table is to be renamed or dropped
+	 */
+	protected $update_table = array();
+
+
+	/**
 	 * This method sets the default dependnacies for this class.
 	 * @param string $table_name The name of the table to be created
 	 * @param string $model_name The name of the model class calling this instance
@@ -123,11 +129,8 @@ class MySQLTable {
 			//get the database table class
 			$table_object = new $class_name();
 
-			//get the table columns
-			$this->getColumns($table_object)->createSQL()->execute();
-
-			//echo "<pre>";print_r($table_object);
-
+			//get the table columns, create SQL and execute
+			return $this->getColumns($table_object)->createSQL()->executeCreate();
 
 		}
 
@@ -137,8 +140,6 @@ class MySQLTable {
 
 
 		}
-		//create instance of the FileHandler
-		$file_handle = FileHandler::Create($this->table_file_path);
 
 	}
 
@@ -149,9 +150,33 @@ class MySQLTable {
 	*/
 	public function updateTable()
 	{
-		//call the method to update table structure in the database
-		return static::Query()->updateTable(static::$table, __CLASS__);
+		//get the instance of the FileInfo class
+		$file_info = FileInfo::Create($this->table_file_path);
 
+		//this file already exists
+		if($file_info->isFile())
+		{
+			//load the clas into momeory
+			include $file_info->getPathname();
+
+			$class_name = substr($file_info->getFilename(), 0,-4);
+
+			//get the database table class
+			$table_object = new $class_name();
+
+			//get the table columns, create SQL and execute
+			$this->getColumns($table_object)->updateSQL()->executeUpdate();
+
+			//echo "{$this->query_string}";exit(0);
+
+		}
+
+		//this file has not been created yet
+		else
+		{
+
+
+		}
 
 	}
 
@@ -177,13 +202,34 @@ class MySQLTable {
 				$column_name =  $property->getName();
 				$comment_array = $this->parseComment($comment_string);
 
-				if ( array_key_exists("@column", $comment_array) )
+				//get meta data for table property
+				if($column_name == 'table')
+				{
+					$class_name = $property->getDeclaringClass()->getName();
+					$this->update_table['rename'] 	= (array_key_exists("@rename", $comment_array)) ? $property->getValue(new $class_name()) : null;
+					$this->update_table['drop'] 	= (array_key_exists("@drop", $comment_array)) ? true : null;
+
+				}
+
+				elseif ( array_key_exists("@column", $comment_array) )
 				{
 					//compose column property
 					$this->columns[$column_name]['primary'] = (array_key_exists("@primary", $comment_array)) ? true : null;
 					$this->columns[$column_name]['type'] 	= (isset($comment_array["@type"])) ? $comment_array["@type"] : null;
 					$this->columns[$column_name]['length'] 	= (isset($comment_array["@length"])) ? $comment_array["@length"] : null;
 					$this->columns[$column_name]['index'] 	= (array_key_exists("@index", $comment_array)) ? true : null;
+
+					if(array_key_exists("@add", $comment_array)){
+
+						$this->columns[$column_name]['add'] = ($comment_array["@add"]) ? $comment_array["@add"] : 'id';
+
+					}
+
+					else $this->columns[$column_name]['add'] = null;
+
+					$this->columns[$column_name]['rename'] 	= (array_key_exists("@rename", $comment_array)) ? $comment_array["@rename"] : null;
+					$this->columns[$column_name]['update'] 	= (array_key_exists("@update", $comment_array)) ? true : null;
+					$this->columns[$column_name]['drop'] 	= (array_key_exists("@drop", $comment_array)) ? $column_name : null;
 
 					if ( ! in_array($this->columns[$column_name]['type'], $this->data_types))	throw new MySQLException(get_class(new MySQLException)." {$this->columns[$column_name]['type']} is not a valid table field data type");
 
@@ -346,11 +392,178 @@ class MySQLTable {
 	}
 
 	/**
+	*This method creates sql string for update queries.
+	*@param null
+	*@return $this
+	*/
+	public function updateSQL()
+	{
+		try{
+
+			//check if table update values are set
+			if($this->update_table['drop'] || $this->update_table['rename'])
+			{
+				//compose query string
+				$this->query_string = ($this->update_table['drop']) ? "DROP TABLE IF EXISTS {$this->table};" : "ALTER TABLE {$this->table} RENAME TO {$this->update_table['rename']};";
+
+				return $this;
+
+			}
+
+			//do column updates
+			else
+			{
+				$lines = array();
+				$template = "ALTER TABLE %s\n %s;";
+				//loop through the columns available
+				foreach($this->columns as $column_name => $column)
+				{
+					//start with less trivial goin upward ADD COLUMN complete DECIMAL(2,1) NULL AFTER description;
+					if($column['add'])
+					{
+						$data_type = $column["type"];//set the column data type
+
+						$length = $column["length"];//set the lenght of the data type
+
+						$primary = ($column["primary"])	? "PRIMARY KEY " : null;//({$column_name})"; //add primary key field
+
+						$index = ($column["index"]) ? "KEY" : null;// {$column_name} ({$column_name})"; //add index field
+
+						switch ($data_type)
+						{
+							case "autonumber":
+
+								$lines[] = "ADD COLUMN {$column_name} INT(11) NOT NULL AUTO_INCREMENT $primary AFTER {$column['add']}\n";
+
+								break;
+
+							case "text":
+
+								if ($length !== null && $length <= 255)	$lines[] = "ADD COLUMN {$column_name} VARCHAR({$length}) DEFAULT NULL AFTER {$column['add']}\n";
+
+								else $lines[] = "ADD COLUMN {$column_name} text DEFAULT NULL  AFTER {$column['add']}\n";
+
+								break;
+
+							case "integer":
+
+								$lines[] = "ADD COLUMN {$column_name} INT(11) DEFAULT NULL AFTER {$column['add']}\n";
+
+								break;
+
+							case "decimal":
+
+								$lines[] = "ADD COLUMN {$column_name} FLOAT DEFAULT NULL AFTER {$column['add']}\n";
+
+								break;
+
+							case "boolean":
+
+								$lines[] = "ADD COLUMN {$column_name} TINYINT(4) DEFAULT NULL AFTER {$column['add']}\n";
+
+								break;
+
+							case "datetime":
+
+								$lines[] = "ADD COLUMN {$column_name} DATETIME DEFAULT NULL AFTER {$column['add']}\n";
+
+								break;
+
+						}
+
+					}
+
+					elseif($column['update'])
+					{
+
+						$data_type = $column["type"];//set the column data type
+
+						$length = $column["length"];//set the lenght of the data type
+
+						$primary = ($column["primary"])	? "PRIMARY KEY " : null;//({$column_name})"; //add primary key field
+
+						$index = ($column["index"]) ? "KEY" : null;// {$column_name} ({$column_name})"; //add index field
+
+						switch ($data_type) //CHANGE COLUMN task_id task_id INT(11) NOT NULL AUTO_INCREMENT;
+						{
+							case "autonumber":
+
+								$lines[] = "CHANGE COLUMN {$column_name} {$column_name} INT(11) NOT NULL AUTO_INCREMENT $primary\n";
+
+								break;
+
+							case "text":
+
+								if ($length !== null && $length <= 255)	$lines[] = "ADD COLUMN {$column_name} VARCHAR({$length}) DEFAULT NULL\n";
+
+								else $lines[] = "CHANGE COLUMN {$column_name} {$column_name} text DEFAULT NULL\n";
+
+								break;
+
+							case "integer":
+
+								$lines[] = "CHANGE COLUMN {$column_name} {$column_name} INT(11) DEFAULT NULL\n";
+
+								break;
+
+							case "decimal":
+
+								$lines[] = "CHANGE COLUMN {$column_name} {$column_name} FLOAT DEFAULT NULL\n";
+
+								break;
+
+							case "boolean":
+
+								$lines[] = "CHANGE COLUMN {$column_name} {$column_name} TINYINT(4) DEFAULT NULL\n";
+
+								break;
+
+							case "datetime":
+
+								$lines[] = "CHANGE COLUMN {$column_name} {$column_name} DATETIME DEFAULT NULL\n";
+
+								break;
+
+						}
+	
+					}
+
+					elseif($column['rename'])
+					{
+						//$lines[] = "CHANGE COLUMN `{$column_name}` `{$column['rename']}`\n";
+					}
+
+					elseif($column['drop'])
+					{
+						$lines[] = "DROP COLUMN {$column['drop']}\n";
+
+
+					}
+
+				}
+
+				$this->query_string = sprintf($template, $this->table, join(',',$lines));
+
+				return $this;
+
+			}
+
+		}
+
+		catch (MySLQException $err){
+
+			$err->errorShow();
+
+		}
+
+	}
+
+	/**
 	*This method executes the query string created.
 	*@param null
 	*@return void
 	*/
-	public function execute()
+	public function executeCreate()
 	{
 		
 		try {
@@ -363,6 +576,30 @@ class MySQLTable {
 
 			if ($result === false) throw new MySQLException(get_class(new MySQLException)." There was an error in the query: {$this->connection->lastError()} <span class=\"query-string\">".$this->query_string."</span>");
 
+			return true;
+				
+		}
+		 catch (MySQLException $e) {
+			
+			$e->errorShow();	
+		}	
+	}	
+
+	/**
+	*This method executes the query string created.
+	*@param null
+	*@return void
+	*/
+	public function executeUpdate()
+	{
+		
+		try {
+
+			$result = $this->connection->execute($this->query_string);
+
+			if ($result === false) throw new MySQLException(get_class(new MySQLException)." There was an error in the query: {$this->connection->lastError()} <span class=\"query-string\">".$this->query_string."</span>");
+
+			return true;
 				
 		}
 		 catch (MySQLException $e) {
