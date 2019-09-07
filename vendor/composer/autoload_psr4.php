0911d3ebc4a2d0b7a121154409f0bd9b20a4e8b1
Geoffrey Okongo <georizhq@gmail.com>
2019-09-07 00:35:50 -0300

    Updating vendor autolaoder

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'Models\\' => array($baseDir . '/application/models'),
     'Libraries\\' => array($baseDir . '/application/libraries'),
+    'Gliver\\' => array($vendorDir . '/gliver/framework/src'),
     'Exceptions\\' => array($baseDir . '/system/Exceptions'),
     'Controllers\\' => array($baseDir . '/application/controllers'),
 );
